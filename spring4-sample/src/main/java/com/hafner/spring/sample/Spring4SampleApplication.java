d1c35bf701219a05c4cf281e0c09a1745e277f26
elena <elenahafner@gmail.com>
2015-02-24 01:23:40 +0000

    First commit

---
+++ spring4-sample/src/main/java/com/hafner/spring/sample/Spring4SampleApplication.java
@@ -0,0 +1,12 @@
+package com.hafner.spring.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring4SampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring4SampleApplication.class, args);
+    }
+}
